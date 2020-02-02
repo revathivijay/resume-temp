b1b7c1e8097dfbdfd86ca466b3fd3f8bde9ca25d
rachanac16 <47475101+rachanac16@users.noreply.github.com>
2020-02-02 08:29:29 +0530

    Update forms.py

--- Main/forms.py
+++ Main/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired,  ValidationError, Email, EqualTo
 
 class findCandidate(FlaskForm):
     job_post = StringField('', validators=[DataRequired()],  render_kw={"placeholder": "Search Job Posts"})
-    submit = SubmitField("Login")
+    submit = SubmitField("Search")
     myChoices = [("date","Filter by date"), ("profession", "Filter by profession"),( "mood","Filter by mood")]
     filters = SelectField("Select Filters", choices = myChoices, render_kw={"text":"Add Filters"})
