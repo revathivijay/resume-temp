eee7cb34bb4cb01231a4a18e41258d1a8508597b
rachanac16 <47475101+rachanac16@users.noreply.github.com>
2020-02-02 08:28:36 +0530

    Update temp.js

--- Main/static/temp.js
+++ Main/static/temp.js
@@ -1,3 +1,6 @@
+
+var global_count = 0;
+
 $(document).ready(function () {
     $('form').on('submit', function (event) {
         console.log('Entered submit')
@@ -16,7 +19,7 @@ $(document).ready(function () {
                 while (node.firstChild) {
                     node.removeChild(node.firstChild);
                 }
-
+                global_count = data.skills.length;
                 for (var i = 0; i < data.skills.length; i++){
                     var btn_id = "btn_" + i;
                     var div_id = "div_" + i;
@@ -38,13 +41,13 @@ $(document).ready(function () {
                 }
 
                 //to add skill
-                var a3 = "<div class='skill_item_subclass'><p class ='col-sm-8 skill_para' id=";
-                var a4 = ">Add Skills here</p><button class = 'col-sm-1 add_btn' id=" + "btn_" + 12 + " onclick='#'>+</button></div >";
+                var a3 = "<div class='skill_item_subclass'><input class ='col-sm-8 skill_para'type = 'text' id=";
+                var a4 = " placeholder = 'Add Skills here'><button class = 'col-sm-1 add_btn' id='btn_add' onclick='add_skill()'>+</button></div >";
                 //creating div element
                 var divv_ = document.createElement("div");
                 divv_.className = 'card add_skill_button';
-                divv_.id = "div_10";
-                divv_.innerHTML = a3 + 10 + a4;
+                divv_.id = "add_div";
+                divv_.innerHTML = a3 + "add_skill_input" + a4;
                 //appending div element in a bigger div element
                 document.getElementById("skill_id").appendChild(divv_);
 
@@ -53,7 +56,34 @@ $(document).ready(function () {
     });
 });
 
+function add_skill() {
 
+    console.log("you want to add a skill");
+    var text = document.getElementById("add_skill_input").value;
+
+    i = global_count;
+    var btn_id = "btn_" + i;
+    var div_id = "div_" + i;
+    var a1 = "<div class='skill_item_subclass'><p class ='col-sm-8 skill_para' id=";
+    var a2 = "></p><button class = 'col-sm-1 cancel_btn' id=" + "btn_" + i + " onclick='cancel_clicked(this.id)'>x</button></div >";
+    //creating div element
+    var divv = document.createElement("div");
+    divv.className = 'card skill_item_class';
+    divv.id = div_id;
+    divv.innerHTML = a1 + i + a2;
+    //appending div element in a bigger div element
+    var add_btn = document.getElementById("add_div");
+    document.getElementById("skill_id").insertBefore(divv, add_btn);
+
+    var text = document.createTextNode(text);
+    var para = document.getElementById(i);
+    console.log("para initialised");
+    para.appendChild(text);
+    console.log("para appended");
+    document.getElementById('add_skill_input').value = "";
+    global_count++;
+
+}
 
 function cancel_clicked(id) {
     console.log(id+"wda");
